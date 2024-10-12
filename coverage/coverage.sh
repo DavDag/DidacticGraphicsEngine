bf6bc09121a079e6ef2adba0e00b8ee8cdd6a83f
Davide Risaliti <davdag24@gmail.com>
2024-10-12 18:26:34 +0200

    coverage support

--- coverage/coverage.sh
+++ coverage/coverage.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 cd ../cmake-build-coverage-ubuntu-clang
-gcovr -r .. --gcov-executable "llvm-cov-18 gcov" --filter /mnt/e/Extra/DidacticGraphicsEngine/engine/src --html --html-details --html-self-contained --html-theme github.dark-green -o ../coverage/coverage_index.html --cobertura ../coverage/coverage_cobertura.xml
+rm -rf ../coverage/html
+rm -rf ../coverage/xml
+mkdir -p ../coverage/html
+mkdir -p ../coverage/xml
+pipx run gcovr -r .. --gcov-executable "llvm-cov-18 gcov" --filter /mnt/e/Extra/DidacticGraphicsEngine/engine/src --html --html-details --html-self-contained --html-theme github.dark-green -o ../coverage/html/coverage_index.html --cobertura ../coverage/xml/coverage_cobertura.xml
