00e411d0bb7bf95dba505faaba321966deacb589
Davide Risaliti <davdag24@gmail.com>
2024-10-12 22:20:53 +0200

    code coverage fixes

--- coverage/coverage.sh
+++ coverage/coverage.sh
@@ -8,4 +8,4 @@ mkdir -p ../coverage/html
 mkdir -p ../coverage/xml
 mkdir -p ../coverage/json
 mkdir -p ../coverage/txt
-pipx run gcovr -r .. --gcov-executable "llvm-cov-18 gcov" --filter /mnt/e/Extra/DidacticGraphicsEngine/engine/src --html --html-details --html-self-contained --html-theme github.dark-green -o ../coverage/html/coverage.html --cobertura ../coverage/xml/coverage.xml --txt ../coverage/txt/coverage.txt --json ../coverage/json/coverage.json
+pipx run gcovr -r .. --gcov-executable "llvm-cov-18 gcov" --filter '\.\./engine/src' --html --html-details --html-self-contained --html-theme github.dark-green -o ../coverage/html/coverage.html --cobertura ../coverage/xml/coverage.xml --txt ../coverage/txt/coverage.txt --json ../coverage/json/coverage.json
