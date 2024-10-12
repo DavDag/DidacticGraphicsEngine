06737c39b57f7fbfd7762ee7fbdf38cc7e147d03
Davide Risaliti <davdag24@gmail.com>
2024-10-12 18:49:08 +0200

    git action fixes

--- coverage/coverage.sh
+++ coverage/coverage.sh
@@ -4,4 +4,4 @@ rm -rf ../coverage/html
 rm -rf ../coverage/xml
 mkdir -p ../coverage/html
 mkdir -p ../coverage/xml
-pipx run gcovr -r .. --gcov-executable "llvm-cov-18 gcov" --filter /mnt/e/Extra/DidacticGraphicsEngine/engine/src --html --html-details --html-self-contained --html-theme github.dark-green -o ../coverage/html/coverage_index.html --cobertura ../coverage/xml/coverage_cobertura.xml
+pipx run gcovr -r .. --gcov-executable "llvm-cov-18 gcov" --filter /mnt/e/Extra/DidacticGraphicsEngine/engine/src --html --html-details --html-self-contained --html-theme github.dark-green -o ../coverage/html/coverage.html --cobertura ../coverage/xml/coverage.xml
