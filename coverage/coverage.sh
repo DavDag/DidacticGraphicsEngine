b7854c0089408d2c10be4661446be7e899d73ff5
Davide Risaliti <davdag24@gmail.com>
2024-10-12 18:55:12 +0200

    git action fixes

--- coverage/coverage.sh
+++ coverage/coverage.sh
@@ -2,6 +2,10 @@
 cd ../cmake-build-coverage-ubuntu-clang
 rm -rf ../coverage/html
 rm -rf ../coverage/xml
+rm -rf ../coverage/txt
+rm -rf ../coverage/json
 mkdir -p ../coverage/html
 mkdir -p ../coverage/xml
-pipx run gcovr -r .. --gcov-executable "llvm-cov-18 gcov" --filter /mnt/e/Extra/DidacticGraphicsEngine/engine/src --html --html-details --html-self-contained --html-theme github.dark-green -o ../coverage/html/coverage.html --cobertura ../coverage/xml/coverage.xml
+mkdir -p ../coverage/json
+mkdir -p ../coverage/txt
+pipx run gcovr -r .. --gcov-executable "llvm-cov-18 gcov" --filter /mnt/e/Extra/DidacticGraphicsEngine/engine/src --html --html-details --html-self-contained --html-theme github.dark-green -o ../coverage/html/coverage.html --cobertura ../coverage/xml/coverage.xml --txt ../coverage/txt/coverage.txt --json ../coverage/json/coverage.json
