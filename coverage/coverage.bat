95cc4248b81dc30cb3e871b339905846f568e5e3
Davide Risaliti <davdag24@gmail.com>
2024-10-12 17:54:22 +0200

    coverage support

---
+++ coverage/coverage.bat
@@ -0,0 +1,1 @@
+wsl -d Ubuntu-24.04 -u davide sh "/mnt/e/Extra/DidacticGraphicsEngine/coverage/coverage.sh"
--- coverage/coverage.sh
+++ coverage/coverage.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env sh
 cd ../cmake-build-coverage-ubuntu-clang
 gcovr -r .. --gcov-executable "llvm-cov-18 gcov" --filter /mnt/e/Extra/DidacticGraphicsEngine/engine/src --html --html-details --html-self-contained --html-theme github.dark-green -o ../coverage/coverage_index.html --cobertura ../coverage/coverage_cobertura.xml
